83ede10b727d0c1b4600d0a601c6198fb2353344
Caleb Wastler <wastler.c@husky.neu.edu>
2018-11-21 13:40:36 -0500

    added in some sudo code comments for where I'm headed

--- node.py
+++ node.py
@@ -9,7 +9,7 @@ import messages
 
 class Node(object):
 
-    def __init__(self, is_leader, leader_hostname, my_hostname, tcp_port=13337, sloppy_Qfrac=0.34):
+    def __init__(self, is_leader, leader_hostname, my_hostname, tcp_port=13337, sloppy_Qfrac=0.34, sloppy_R=1, sloppy_W=3):
 
         self.is_leader = is_leader
         self.leader_hostname = leader_hostname
@@ -23,8 +23,11 @@ class Node(object):
         self.sloppy_Qfrac = sloppy_Qfrac  # fraction of total members to replicate on
 
         # sets self.sloppy_Qsize to the number of replications required
-        self.update_SQsize = lambda: floor(len(self.membership_ring) * self.sloppy_Qfrac)
+        self.update_SQsize = lambda: self.sloppy_Qsize=floor(len(self.membership_ring) * self.sloppy_Qfrac)
         self.update_SQsize()
+        #number of peers required for a read or write to succeed.
+        self.sloppy_R=sloppy_R
+        self.sloppy_W=sloppy_W
 
         # saves all the pending membership messages
         self._membership_messages = defaultdict(set)
@@ -103,6 +106,7 @@ class Node(object):
 
         self.membership_ring.add_node(data[0], data[0])  # node id is same as hostname for now
 
+        self.update_SQsize()
         # todo: if number of replicas goes up, need to find new peer and send them your files
 
         return "added " + data[0] + " to ring"
@@ -113,6 +117,7 @@ class Node(object):
 
         self.membership_ring.remove_node(data[0])
 
+        self.update_SQsize()
         # todo: update sloppy quorum size, if size changes
         # tell your lowest index replica to delete your files
 
@@ -129,11 +134,16 @@ class Node(object):
         target_node = self.membership_ring.get_node_for_key(data[0])
 
         if target_node == self.my_hostname:
-            self.db.storeFile(key, self.my_hostname, prev, value)
 
             # todo: sloppy quorum
 
+            #send store command to all nodes that should replicate
+
+            #if at least sloppy_W-1 respond
+            self.db.storeFile(key, self.my_hostname, prev, value)
             return "stored %s:%s locally [%s]" % (key, value, self.my_hostname)
+        
+            #otherwise, write failed
         else:
             return self._send_data_to_peer(target_node, key, value)
 
@@ -145,9 +155,15 @@ class Node(object):
         target_node = self.membership_ring.get_node_for_key(data[0])
 
         if target_node == self.my_hostname:
+            #if sloppy_R == 1
             return "retrieved %s:%s locally [%s]" % (
                 data[0], self.db.getFile(data[0]), self.my_hostname
             )
+
+            #otherwise, send read request to all replicas
+                #when you get R-1 responses, send each unique response back 
+                #to the client
+
         else:
             return self._get_data_from_peer(target_node, data[0])
 
@@ -165,9 +181,26 @@ class Node(object):
             return self._delete_data_from_peer(target_node, data[0])
 
     def _send_data_to_peer(self, target_node, key, value):
+
+        #forward request to correct peer
+
+        #if you are one of the replicas, wait for peer message
+            #and respond to it
+
+        #wait for correct peer to send you confirmation or failue
+            #for the write op
+
         return "stored %s:%s on node %s" % (key, value, target_node)
 
     def _get_data_from_peer(self, target_node, key):
+
+        #forward request to correct peer
+
+        #if you are one of the replicas wait for the peer message 
+            #and respond to it
+
+        #wait for peer to send back all results or a failure
+
         return "retrieved %s from node %s" % (target_node, key)
 
     def _delete_data_from_peer(self, target_node, key):
