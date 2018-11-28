337f04e676e5182fb031ef6b0f81530a30ca7f4c
Vamsi Atluri <vamc19@gmail.com>
2018-11-28 05:12:54 -0500

    remove redundant arguments and add sloppy quorum arguments

--- dynamo.py
+++ dynamo.py
@@ -5,8 +5,7 @@ from node import Node
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('--peer', help='Hostname of a known peer in the system')
-    parser.add_argument('--peer_port', help='Port used by a known peer in the system')
+    parser.add_argument('--leader', help='Hostname of leader')
     parser.add_argument('--port', default=13337, type=int, help='TCP port number for server socket')
     parser.add_argument('--qfrac', default=0.34, type=float, help='Fraction of peers on which data will be replicated')
     parser.add_argument('--sq_write_n', default=0.34, type=float, help='Min number of confirmed peers in a put operation with sloppy quorum')
@@ -17,12 +16,12 @@ if __name__ == '__main__':
     is_leader = False  # is the current node the leader
     hostname = socket.gethostname()
 
-    peer_hostname = args.peer
-    if not peer_hostname or (peer_hostname == hostname):
+    leader_hostname = args.leader
+    if not leader_hostname or (leader_hostname == hostname):
         is_leader = True
-        peer_hostname = hostname
+        leader_hostname = hostname
 
-    n = Node(is_leader, leader_hostname, hostname, tcp_port=args.port, 
-            sloppy_Qfrac=args.qfrac, sloppy_R=args.sq_read_n, sloppy_W=args.sq_write_n)
-    
-    n.start()
+    n = Node(is_leader, leader_hostname, hostname, tcp_port=args.port,
+             sloppy_Qfrac=args.qfrac, sloppy_R=args.sq_read_n, sloppy_W=args.sq_write_n)
+
+    n.accept_connections()
